788ef515c713999ec77321473c816cd14236cf3f
Mayank Padshala <mayankpadshala@Mayanks-MacBook-Air.local>
2021-07-06 15:19:09 -0700

    Sum and Multiply of the list added

---
+++ sum_multiply_list.py
@@ -0,0 +1,12 @@
+list1 = [1,3,5,7,9,11]
+print("Original List: ", list1)
+
+add_list = 0
+for item1 in list1:
+    add_list = add_list + item1
+print("Total after multiply the list:", add_list)
+
+mutli_list = 1
+for item in list1:
+    mutli_list = mutli_list * item
+print("Total after multiply the list: ",mutli_list)
