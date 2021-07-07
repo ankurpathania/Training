57a5203e771ae9e712c3a4c262fe53dac4deeaf4
Mayank Padshala <mayankpadshala@Mayanks-MacBook-Air.local>
2021-07-07 11:10:56 -0700

    Nothing edit, just for practice

--- remove_even_number.py
+++ remove_even_number.py
@@ -5,7 +5,7 @@ for i in range(0,list1):
     ele = int(input())
     lst.append(ele)
 print("List: ",lst)
-lst = [x for x in lst if x%2!=0]
 
+lst = [x for x in lst if x%2!=0]
 print("List without even number: ",lst)
 
